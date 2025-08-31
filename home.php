2bdbaffa6c1c589c84dacf3497765d2a6b3464de
Mas Jihan Afra Auzia <h1101241011@student.untan.ac.id>
2025-08-31 11:37:47 +0700

    Update and rename Dockerfile to home.php

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php
+// home.php
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Home</title>
+</head>
+<body>
+    <h1>Selamat datang di halaman Home!</h1>
+    <p>Ini adalah contoh halaman PHP sederhana.</p>
+</body>
+</html>
